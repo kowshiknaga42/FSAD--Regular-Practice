4e6fc815b1fae3945974209ea330db4833022b43
sarathsurya062007@gmail.com <koushiknaga42@gmail.com>
2026-02-06 12:40:38 +0530

    Convert hibernate-create to normal folder

---
+++ hibernate-create/src/main/java/com/klu/MainApp.java
@@ -0,0 +1,32 @@
+package com.klu;
+import org.hibernate.*;
+import org.hibernate.cfg.Configuration;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		//Load configuration & Create the SessionFactory
+		SessionFactory factory=new Configuration().configure().buildSessionFactory();
+		
+		//open session
+		Session session=factory.openSession();
+		
+		//Begin Transaction
+		Transaction tx=session.beginTransaction();
+		
+		//create the table/object
+		Student s=new Student("Ravi");
+		
+		//save the data/object
+		session.save(s);
+		
+		//commit
+		tx.commit();
+		//close the connection
+		session.close();
+		factory.close();
+		
+
+	}
+
+}
